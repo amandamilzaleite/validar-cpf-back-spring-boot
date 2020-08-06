574f9423acece1b8bacefa029d61142764de1893
amandamilzaleite <amandamilzaleite@gmail.com>
2020-08-06 14:06:11 -0300

    criando rota home

--- src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
+++ src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
@@ -18,5 +18,10 @@ public class ValidadorController {
 		}
 		 return "{\"status\": \"invalido\"}";
 	}
+	@GetMapping("/")
+	public String index() {
+		
+		 return "{\"mensagem\": \"Bem vindo a API Turma 6A Itau\"}";
+	}
 
 }
